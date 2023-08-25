2c5113dbcec124e9e8bb0bf6cf98b67ad9886635
Gerasim <g-926@ya.ru>
2023-08-25 13:40:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragments.databinding.ActivityMainBinding
+
+// наследуемся от AppCompatActivity, для поддержки Fragment
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // проверяем, если Activity пересоздаётся, то Fragment подгрузится автоматически
+        if (savedInstanceState == null) {
+            // в этот момент мы показываем Fragment со списком городов
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container_view, CitiesFragment())
+                .commit()
+        }
+
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.replaceCountryFragment.setOnClickListener {
+            // в этот момент мы заменяем Fragment со списком городов на Fragment со списком стран
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, CountriesFragment<Any>())
+                .setReorderingAllowed(true)
+                .commit()
+
+        }
+        binding.replaceBackStackCountryFragment.setOnClickListener {
+            // в этот момент мы заменяем Fragment со списком городов на Fragment со списком стран, при этом сохраняя Fragment со списком городов в Back Stack
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, CountriesFragment<Any>())
+                .addToBackStack("countries")
+                .setReorderingAllowed(true)
+                .commit()
+        }
+    }
+}
